a4b721ee08d0755ba33fb56ee253f7eaf57b44bd
Projava951 <codemasterrobin95@gmail.com>
2023-06-16 19:45:46 +0100

    jacob commit

--- extract_category_links.py
+++ extract_category_links.py
@@ -10,6 +10,17 @@ class Task(BaseTask):
         def htmltosoup(page):
             return BeautifulSoup(page, 'html.parser')
 
+        def get_leaf_links(div):
+            if(div.find_all("div", class_ = 'ml-2') == []):
+                return [div.select_one('a[class$=" link"]').href]
+
+            leaf_links = []
+
+            for div in div.find_all("div", class_ = 'ml-2'):
+                leaf_links.extend(get_leaf_links(div))
+            
+            return leaf_links
+
         def get_category_links(input_categories):
             url = "https://www.g2.com/categories?view_hierarchy=true"
             driver.organic_get(url);
@@ -22,11 +33,20 @@ class Task(BaseTask):
             driver.get_element_or_none_by_selector('div.newspaper-columns', Wait.VERY_LONG * 4)
             html = htmltosoup(driver.page_source)
             category_list = html.find_all("div", class_ = 'newspaper-columns__list-item pb-1')
-
+            category_links = []
             for div in category_list:
                 h2_name = div.select_one("h2").text
-                print(h2_name)
+                is_exist = False
 
+                for input_category in input_categories:
+                    if (input_category == h2_name):
+                        is_exist = True
+                        break
+
+                if (is_exist):
+                    category_links.extend(get_leaf_links(div))
+                        
+            print(category_links)
             return "asdf"
 
         input_categories = Output.read_pending()
