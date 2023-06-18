2d122a5a7f22fae3d611f900d5b578cf04ecfbc6
Projava951 <codemasterrobin95@gmail.com>
2023-06-18 14:35:01 +0100

    jacob commit

--- extract_product_links.py
+++ extract_product_links.py
@@ -27,6 +27,45 @@ class Task(BaseTask):
             input_category = self.input_category
             ol_ele = html.select_one('ol[id$="breadcrumbs"]')
             li_eles = ol_ele.find_all("li", itemprop = 'itemListElement')
+            all_categories = ""
+            for i in range(len(li_eles)):
+                if (i > 0 and i < len(li_eles) - 1):
+                    if (i == len(li_eles) - 2):
+                        all_categories += li_eles[i].select_one('span[itemprop$="name"]').text
+                    else:
+                        all_categories += li_eles[i].select_one('span[itemprop$="name"]').text + ' | '
+
+            company_link = company_url
+            title_div = html.select_one('div[class$="product-head__title"]')
+            title = title_div.select_one('div[itemprop$="name"]').select_one('a').text
+
+            details_list = html.find_all("div", class_ = 'ml-1')
+
+            
+            details_titles = ['LinkedIn Page' if 'LinkedIn' in p.next.text and 'Page' in p.next.text else  p.next.text for p in details_list]
+
+            details_values = []
+            
+            for p in details_list:
+                
+                p.find("div", class_ = 'fw-semibold').decompose()
+                link = p.find("a", class_ = 'link')
+            
+                if link is not None:
+                    p = link['href']
+                else:
+                    data = str(p)
+                    
+                    if 'Twitter' in data:
+                        p =  data.replace('<div class="ml-1">', '').replace('</div>', '').replace('<br/>', ' ').strip()
+                    else:
+                        p = p.text
+            
+                details_values.append(p)
+
+            details = dict(zip(details_titles, details_values))
+
+            print(details)
 
             return li_eles
 
